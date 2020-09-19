8a6887679f573914f0c1a45d5ef2be29c22225ab
Loh Siu Yin <siuyin@beyondbroadcast.com>
2020-09-19 14:49:44 +0800

    Reformatted code.

--- sample/stm8/blink/blink.c
+++ sample/stm8/blink/blink.c
@@ -13,27 +13,28 @@
 
 /* Simple busy loop delay */
 void delay(unsigned long count) {
-    while (count--)
-        nop();
+	while (count--) {
+		nop();
+	}
 }
 
-int main(void)
-{
-    /* Set clock to full speed (16 Mhz) */
-    CLK_CKDIVR = 0;
+int main(void) {
+	/* Set clock to full speed (16 Mhz) */
+	CLK_CKDIVR = 0;
 
-    /* GPIO setup */
-    // Set pin data direction as output
-    PORT(LED_PORT, DDR)  |= LED_PIN; // i.e. PB_DDR |= (1 << 5);
-    // Set pin as "Push-pull"
-    PORT(LED_PORT, CR1)  |= LED_PIN; // i.e. PB_CR1 |= (1 << 5);
+	/* GPIO setup */
+	// Set pin data direction as output
+	PORT(LED_PORT, DDR)  |= LED_PIN; // i.e. PB_DDR |= (1 << 5);
+	// Set pin as "Push-pull"
+	PORT(LED_PORT, CR1)  |= LED_PIN; // i.e. PB_CR1 |= (1 << 5);
 
 	while(1) {
-        // LED on
-        PORT(LED_PORT, ODR) |= LED_PIN; // PB_ODR |= (1 << 5);
-        delay(100000L);
-        // LED off
-        PORT(LED_PORT, ODR) &= ~LED_PIN; // PB_ODR &= ~(1 << 5);
-        delay(300000L);
-    }
+		// LED on
+		PORT(LED_PORT, ODR) |= LED_PIN; // PB_ODR |= (1 << 5);
+		delay(100000L);
+
+		// LED off
+		PORT(LED_PORT, ODR) &= ~LED_PIN; // PB_ODR &= ~(1 << 5);
+		delay(300000L);
+	}
 }
